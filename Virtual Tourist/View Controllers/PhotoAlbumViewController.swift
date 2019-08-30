066abbce7b2111f3d2c53745c87be7910eadc0e5
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-08-30 16:48:09 +0100

    feat: Storyboard setup complete. Setup controllers and link IB outlets and actions. app icons.

---
+++ Virtual Tourist/View Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PhotoAlbumView.swift
+//  Virtual Tourist
+//
+//  Created by Sean Williams on 30/08/2019.
+//  Copyright © 2019 Sean Williams. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var newCollectionButton: UIBarButtonItem!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func newCollectionButtonTapped(_ sender: Any) {
+    }
+}
---
+++ Virtual Tourist/View Controllers/TravelLocationsMapViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TravelLocationsMapView.swift
+//  Virtual Tourist
+//
+//  Created by Sean Williams on 30/08/2019.
+//  Copyright © 2019 Sean Williams. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var editButton: UIBarButtonItem!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(addAnnotationOnLongPress(gesture:)))
+        longPressGesture.minimumPressDuration = 1
+        self.mapView.addGestureRecognizer(longPressGesture)
+    }
+
+    
+    @objc func addAnnotationOnLongPress(gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            let point = gesture.location(in: mapView)
+            let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+            print(coordinate)
+            
+            var annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        performSegue(withIdentifier: "photoAlbum", sender: self)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // code to center user on map pin
+//        let location = locations.last
+//        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: (location?.coordinate.longitude)!)
+//        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)) //zoom on map
+//        self.mapView.setRegion(region, animated: true)
+    }
+    
+    @IBAction func editButtonTapped(_ sender: Any) {
+    }
+    
+}
+
